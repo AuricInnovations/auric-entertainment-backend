ac9f0d4027a3897aaa299721e2ed6ca666981ee3
Rusiru <rusirudesilva@gmail.com>
2025-11-01 02:25:12 +0800

    Initial commit

---
+++ auric-backend/src/main/java/com/auric/entertainment/auric_backend/AuricEntertainmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.auric.entertainment.auric_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuricEntertainmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuricEntertainmentBackendApplication.class, args);
+	}
+
+}
